43a14636c5dd5e154bd60ec240dd75b0f306f438
beerrta <61797652+beerrta@users.noreply.github.com>
2020-06-15 00:32:25 +0200

    Add files via upload

---
+++ poblacions.m
@@ -0,0 +1,37 @@
+function [pob]=poblacions(portes,poblacio)
+n=1;
+pob=[];
+%portes=8
+while n<=poblacio %el 20 aquest sera la mida de la població
+    A=zeros(portes+3,portes+3);
+   
+for i=1:length(A(:,1)) %recorro files
+    for j=1:length(A(1,:)) %recorro columnes
+    if i==4%les 3 primeres columnes son totes de 0
+        d=randi(2:3); % per saber si començem amb 3 cables o amb 2
+        if d==3
+            cab=[1,1,1];
+        elseif  d==2
+            cab=[1,1,0];
+        end
+        a=cab(randperm(length(cab)));
+        A(1,4)=a(1);
+        A(2,4)=a(2);
+        A(3,4)=a(3);
+    elseif i>4 % posem 1 de manera random en posicions de la nostra matiu. tenint en compte que no es possible tenir un 1 en una fila si no esta mes enlla de la columna igual el numero de fila
+ %el 5 es perque en la porta 4 hi ha els cables llavors a partir de la 5ena posem els cables randoms ja uqe en la 4 ho hem fet anteriormet
+            if i>j && sum(A(:,i-1))
+                A(j,i)=round(rand(1));
+            end
+    end  
+    end 
+end
+pob(:,:,n)=A;
+n=n+1;
+end
+end
+        
+
+    
+    
+    
--- recombinacio.m
+++ recombinacio.m
@@ -1,14 +1,14 @@
 function recombinacio_matriu=recombinacio(poblacio,portes)
 recombinacio_matriu=[];
-i=0;%laparellament el farem random nomes amb la primera meitat de individus
+l=0;%laparellament el farem random nomes amb la primera meitat de individus
 %mes fit ja que volem trobar optim i laparellament amb les menys fit ens allunyaria dell
 o=1;
-while i<length(poblacio)/2
+while l<length(poblacio)/2
     p=length(poblacio)/2;%Así escoge entre la mitad mejor
-    ind1=round((p-1)*rand)+1;
+    ind1=round((p-l)*rand)+1;
     %ind1=round(rand(1)*p);
     A=poblacio(:,:,ind1);%fem que dos individus s'aparellin de forma aleatoria, un cop s'han aparellat, ja no es poden aparella mes
-    %poblacio(ind1)=[];%un cop lindividu seleccionat el borrem ja que no volem que es repeteixin el progenitors
+    %(ind1)=[];%un cop lindividu seleccionat el borrem ja que no volem que es repeteixin el progenitors
     ind2=round((p-1)*rand)+1;
     %ind2=round(rand*(length(poblacio)-1-(i-2)))+1;
     B=poblacio(:,:,ind2);
@@ -17,13 +17,13 @@ while i<length(poblacio)/2
         for n=1:portes
             if m>n
                random=rand(1);
-               if rand>0.5% tenim un 50% de probs de que la desendencia tingui part del pare o de la mare 
+               if random>0.5% tenim un 50% de probs de que la desendencia tingui part del pare o de la mare 
                    A(n,m)=B(n,m);
                end
             end
         end
     end
-    i=i+1;
+    l=l+1;
     recombinacio_matriu(:,:,o)=A;
     o=o+1;
 end
